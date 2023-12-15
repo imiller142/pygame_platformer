aa1d34fa6ab4fbdb56c661da30783082766dc1ab
Ian Miller <imiller142@gmail.com>
2023-12-15 15:59:34 -0600

    working on movement

--- game.py
+++ game.py
@@ -22,17 +22,36 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0, 0, 0))
 
-        self.img_pos = [100, 260]        
+        self.img_pos = [100, 260]
+        self.movement = [False, False]
+
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
 
     def run(self):
         while True:
-            screen.blit(self.img, self.img_pos)
+            screen.fill((14, 219, 248))
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.screen.blit(self.img, self.img_pos)
+
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+
 
             pygame.display.update()
             self.clock.tick(60)
